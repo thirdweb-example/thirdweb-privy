b39a31b1dce0c8209f42b1cce8fcaed3639ce756
Joaquim Verges <joaquim.verges@gmail.com>
2024-08-22 20:37:56 +1200

    simple smart wallet integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,56 +2,82 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { PrivyProvider } from "@privy-io/react-auth";
+import { ThirdwebProvider } from "thirdweb/react";
+import { createThirdwebClient } from "thirdweb";
+import { sepolia } from "thirdweb/chains";
+
+export const client = createThirdwebClient({
+	clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/AdelleSans-Regular.woff"
-          as="font"
-          crossOrigin=""
-        />
-        <link
-          rel="preload"
-          href="/fonts/AdelleSans-Regular.woff2"
-          as="font"
-          crossOrigin=""
-        />
-        <link
-          rel="preload"
-          href="/fonts/AdelleSans-Semibold.woff"
-          as="font"
-          crossOrigin=""
-        />
-        <link
-          rel="preload"
-          href="/fonts/AdelleSans-Semibold.woff2"
-          as="font"
-          crossOrigin=""
-        />
+	return (
+		<>
+			<Head>
+				<link
+					rel="preload"
+					href="/fonts/AdelleSans-Regular.woff"
+					as="font"
+					crossOrigin=""
+				/>
+				<link
+					rel="preload"
+					href="/fonts/AdelleSans-Regular.woff2"
+					as="font"
+					crossOrigin=""
+				/>
+				<link
+					rel="preload"
+					href="/fonts/AdelleSans-Semibold.woff"
+					as="font"
+					crossOrigin=""
+				/>
+				<link
+					rel="preload"
+					href="/fonts/AdelleSans-Semibold.woff2"
+					as="font"
+					crossOrigin=""
+				/>
 
-        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
-        <link rel="manifest" href="/favicons/manifest.json" />
+				<link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+				<link rel="icon" href="/favicons/icon.svg" type="image/svg+xml" />
+				<link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+				<link rel="manifest" href="/favicons/manifest.json" />
 
-        <title>Privy Auth Starter</title>
-        <meta name="description" content="Privy Auth Starter" />
-      </Head>
-      <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
-        config={{
-          embeddedWallets: {
-            createOnLogin: "all-users",
-          },
-        }}
-      >
-        <Component {...pageProps} />
-      </PrivyProvider>
-    </>
-  );
+				<title>Privy Auth Starter</title>
+				<meta name="description" content="Privy Auth Starter" />
+			</Head>
+			<PrivyProvider
+				appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
+				config={{
+					supportedChains: [
+						{
+							...sepolia,
+							name: "Sepolia",
+							nativeCurrency: {
+								name: "Sepolia Ether",
+								symbol: "ETH",
+								decimals: 18,
+							},
+							blockExplorers: {
+								default: {
+									...sepolia.blockExplorers[0],
+								},
+							},
+							rpcUrls: { default: { http: [sepolia.rpc] } },
+						},
+					],
+					embeddedWallets: {
+						createOnLogin: "users-without-wallets",
+					},
+				}}
+			>
+				<ThirdwebProvider>
+					<Component {...pageProps} />
+				</ThirdwebProvider>
+			</PrivyProvider>
+		</>
+	);
 }
 
 export default MyApp;
